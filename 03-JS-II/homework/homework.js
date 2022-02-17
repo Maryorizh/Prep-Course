70d6996ef08cce18cc5e903d51967f3fc6bd889e
Maryorizh <maryorizapatah@hotmail.com>
2022-02-17 08:37:29 -0500

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -128,53 +128,64 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  switch (num1, num2, num3){
- case "num1 > num2 && num1 > num3 && num1 > 0":
-    return "Número 1 es mayor y positivo";
-  case "num3 > num1 && num3 > num2":
-  var restres = num3++
-  return restres;
-  case "num1 || num2 || num3 === 0":
-    return "Error";
-  case "num1 || num2 || num3 < 0":
-    return "Hay negativos";}
-  return false;
+  if ( num1 < 0 || num2 < 0 || num3 <0){
+    return "Hay negativos"
   }
-
-
-function esPrimo(numero) {
-  // Devuelve "true" si "numero" es primo
-  // De lo contrario devuelve "falso"
-  // Pista: un número primo solo es divisible por sí mismo y por 1
-  // Pista 2: Puedes resolverlo usando un bucle `for`
-  // Nota: Los números 0 y 1 NO son considerados números primos
-
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  if (num3 > num1 && num3 > num2) {
+    var resultadotres = num3 + 1;
+    return resultadotres;
+  }
+  if (num1 > num2 && num1 > num3 && num2 > 0){
+    return "Número 1 es mayor y positivo";
+  }
+  return false
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+if (valor === true ){
+  return "Soy verdadero"
+}
+return "Soy falso"
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí  
+  var tabla6 = [];
+ for (let i= 0; i<11; i++) {
+  tabla6.push (i*6)
+   return tabla6
+ }
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero >99 && numero < 1000 ) {
+    return true
+  }
+  return false
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+  do {
+numero = numero + 5;
+i = i + 1
+  }
+while ( i < 8 ); {
+  return numero
+}
 }
 
 
@@ -192,7 +203,6 @@ module.exports = {
   esEntero,
   fizzBuzz,
   operadoresLogicos,
-  esPrimo,
   esVerdadero,
   tablaDelSeis,
   tieneTresDigitos,
